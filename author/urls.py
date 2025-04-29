fadc7a09d4fb58ae9acc8ca1f0db244d0b8a0f59
Maksym <maximuschampion2002@gmail.com>
2025-04-29 18:40:54 +0300

    add serializer & models

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,11 @@
-# Create your urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r"authors", AuthorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
