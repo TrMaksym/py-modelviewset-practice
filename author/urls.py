27ae220e17a57857fbfe19bf5a062f2caf321614
Maksym <maximuschampion2002@gmail.com>
2025-04-29 18:57:00 +0300

    change in urls basename

--- author/urls.py
+++ author/urls.py
@@ -6,7 +6,7 @@ from .views import AuthorViewSet
 app_name = "author"
 
 router = DefaultRouter()
-router.register(r"manage", AuthorViewSet, basename="manage")
+router.register(r"author", AuthorViewSet, basename="author")
 
 urlpatterns = [
     path("", include(router.urls)),
