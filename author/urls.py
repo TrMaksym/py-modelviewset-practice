86c6809ae56c0f1dc82893f1db1a93af6603f597
Maksym <maximuschampion2002@gmail.com>
2025-04-29 18:45:05 +0300

    fix errors

--- author/urls.py
+++ author/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet
 
+
+app_name = "author"
+
 router = DefaultRouter()
 router.register(r"authors", AuthorViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
 ]
-
